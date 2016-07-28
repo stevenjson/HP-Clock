b6edef30f99f11143e54bde326be1fdac30030b7
Steven Jorgensen <spjorgensen94@gmail.com>
2016-07-28 19:50:27 -0400

    Added startup file for sensitive data

--- loc_data.py
+++ loc_data.py
@@ -1,6 +1,21 @@
 import time
 import subprocess
+try:
+	start_file = open("startup.csv")
+	#InfoLoc = start_file.readline().split(',')
+	#enter mosquitto
+	#startup = subprocess.Popen("mosquitto_sub --cert " + InfoLoc[0] + " --key " + InfoLoc[1] + " --cafile " + 
+	#			   InfoLoc[2] + " -t 'owntracks/#' -h " + InfoLoc[3] + " -p " + InfoLoc[4] , stdout=subprocess.PIPE)
 
-process = subprocess.Popen("tail -n 1 data/output", stdout=subprocess.PIPE)
-output = process.communicate()[0]
-print (output)
+	start_file.close()
+
+	while True:
+		process = subprocess.Popen("tail -n 1 data/output", stdout=subprocess.PIPE)
+		output = process.communicate()[0]
+
+		data_list = str(output)
+		data_list = data_list.split(',')
+
+except KeyboardInterrupt:
+	#Exit mosquitto 
+        print("EXIT")
